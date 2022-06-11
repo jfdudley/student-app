caf05b0b443f4cba31377a7bdc9718bc150c57c3
Joanna Dudley <jfdudleybmc@gmail.com>
2022-06-10 21:01:53 -0400

    Completed React Components lesson

---
+++ src/components/StudentList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./StudentList.css";
+
+const StudentList = () => {
+  return (
+    <section>
+      <h2>Student List</h2>
+      <ul>
+        <li>Student A</li>
+        <li>Student B </li>
+        <li>Student C</li>
+      </ul>
+    </section>
+  );
+};
+
+export default StudentList;
